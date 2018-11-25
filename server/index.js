ad66aff0d3c209b2b683d94f5d381b16720ba2f2
Paul Brower <pabrower@icloud.com>
2018-11-24 22:21:00 -0800

    initial server setup and deployment

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express"); //using common js modules for serverside of app will use ES2015_modules for frontend
+const app = express(); //used to setup config to listen for incoming requests and route them to various route handlers
+
+app.get("/greeting", (req, res) => {
+  res.send({ hi: "there" });
+});
+
+app.listen(5000);
