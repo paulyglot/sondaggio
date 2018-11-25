94bea3b31559307995855db656e148b3dfdaa214
Paul Brower <pabrower@icloud.com>
2018-11-25 13:21:55 -0800

    redoing deployment

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express"); //using common js modules for serverside of app will use ES2015_modules for frontend
-const app = express(); //used to setup config to listen for incoming requests and route them to various route handlers
-
-app.get("/greeting", (req, res) => {
-  res.send({ hi: "there" });
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT);
