7f13f8de0cce44c621e713caeee819502bda09f3
Paul Brower <pabrower@icloud.com>
2018-11-25 20:27:18 -0800

    refactored and OAuth connection complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ Paul: "my name" });
-});
+require("../services/passport");
+require("../routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
