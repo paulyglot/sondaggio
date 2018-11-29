d96d2cee0d7f5fa4ac459cd46a9bb18b6975ddcb
Paul Brower <pabrower@icloud.com>
2018-11-29 14:46:27 -0800

    updated heroku scripts for server prod deploy

--- src/index.js
+++ src/index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 require("../routes/authRoutes")(app);
 require("../routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === 'production') {
+  //Ensures Express will serve up production assets like main.js/main.css
+  app.use(express.static('client/build'));
+
+  //Ensures Express will serve up index.html if doesn't recognize router
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
