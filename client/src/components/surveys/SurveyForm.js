557d32578ad8ac8bdd409a7bc43adef5b9a6cdd6
Paul Brower <pabrower@icloud.com>
2018-12-02 15:28:15 -0800

    completed survey form refactoring

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,8 @@ import _ from "lodash";
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import SurveyField from "./SurveyField";
+import { Link } from "react-router-dom";
+import validateEmails from '../../utils/validateEmails';
 
 const FIELDS = [
   { label: "Survey Title", name: "title" },
@@ -30,13 +32,34 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  errors.emails = validateEmails(values.emails || '');
+
+  _.each(FIELDS, ({ name }) => {
+    if (!values[name]) {
+      errors[name] = "You must enter input";
+    }
+  });
+
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: "surveyForm"
 })(SurveyForm);
---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,14 @@
+const regExpression = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(",")
+    .map(email => email.trim())
+    .filter(email => regExpression.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+
+  return;
+};
