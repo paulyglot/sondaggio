cd2af1684f16bd327656f66fcf8a1becf3b7998d
Paul Brower <pabrower@icloud.com>
2018-12-03 21:17:11 -0800

    added form review feature

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
-import SurveyForm from './SurveyForm';
-import SurveyFormReview from './SurveyFormReview';
+import React, { Component } from "react";
+import { reduxForm } from 'redux-form';
+import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
   state = { showFormReview: false };
 
   renderContent() {
     if (this.state.showFormReview === true) {
-       return <SurveyFormReview
-        onCancel={( => this.setState)}
-       />;
+      return (
+        <SurveyFormReview
+          onCancel={() => this.setState({ showFormReview: false })}
+        />
+      );
     }
 
     return (
       <SurveyForm
-      onSurveySubmit={() => this.setState({ showFormReview: true })}
+        onSurveySubmit={() => this.setState({ showFormReview: true })}
       />
     );
   }
 
   render() {
-    return (
-      <div>
-        {this.renderContent()}
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
