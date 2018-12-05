1fd51ad3e18dcd7480791a707feb4520f36fce43
Paul Brower <pabrower@icloud.com>
2018-12-05 00:54:43 -0800

    debugging sendgrid production

--- src/config/prod.js
+++ src/config/prod.js
@@ -5,7 +5,7 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
   sendGridKeyProd: process.env.SEND_GRID_KEY_PROD,
+  sendGridKey: process.env.SEND_GRID_KEY,
   redirectDomain: process.env.REDIRECT_DOMAIN
 };
