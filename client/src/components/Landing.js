f419786702b123c5bde0b3862173245dfddeb2fe
Paul Brower <pabrower@icloud.com>
2018-12-07 00:25:53 -0800

    added survey creation to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return  (
@@ -6,7 +7,15 @@ const Landing = () => {
       <h1>
         Sondaggio
       </h1>
-      Collect feedback from your users
+      To get started click the big green button above to add credits!
+      <div>
+      If you already have credits, click the red button below to create your survey.
+      </div>
+      <div className="fixed-action-btn">
+      <Link to='/surveys/new' className="btn-floating btn-large red">
+      <i className="material-icons">library_add</i>
+      </Link>
+      </div>
     </div>
   )
 }
